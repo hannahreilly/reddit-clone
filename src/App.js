ce83583ceab4364350bc3c7af1fb1c4e6efbaf69
Hannah Reilly <reillyhannah65@gmail.com>
2019-12-12 11:01:58 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: "",
+      posts: []
+    }
+  }
+  
+  onChange = (e) => {
+    const content = e.target.value;
+    this.setState({
+      content
+    })
+  }
+
+  postSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.postSubmit}>
+          <input
+            type="textarea"
+            name="content"
+            value={this.state.content}
+            placeholder="Post It"
+            onChange={this.onChange}
+          />
+          <input
+            type="submit"
+            value="post" 
+            />
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
